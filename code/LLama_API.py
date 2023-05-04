f435623ba52fc543f08f1ba9f958e7d1335d0f7d
Afra Baas <lcur1101@test3.irc.sara.nl.irc.sara.nl>
2023-05-04 11:22:45 +0200

    delete later

--- code/LLama_API.py
+++ code/LLama_API.py
@@ -1,16 +1,12 @@
-# from transformers import LlamaForCausalLM, LlamaTokenizer
+from transformers import AutoTokenizer, AutoModelForMaskedLM
 
-# tokenizer = LlamaTokenizer.from_pretrained("/output/path")
-# model = LlamaForCausalLM.from_pretrained("/output/path")
+tokenizer = AutoTokenizer.from_pretrained('xlm-roberta-base')
+model = AutoModelForMaskedLM.from_pretrained("xlm-roberta-base")
 
+# prepare input
+text = "Replace me by any text you'd like."
+encoded_input = tokenizer(text, return_tensors='pt')
 
-# from transformers import LlamaModel, LlamaConfig
-
-# # Initializing a LLaMA llama-7b style configuration
-# configuration = LlamaConfig()
-
-# # Initializing a model from the llama-7b style configuration
-# model = LlamaModel(configuration)
-
-# # Accessing the model configuration
-# configuration = model.config
+# forward pass
+output = model(**encoded_input)
+print(output)
